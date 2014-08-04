576820d8d9422d0bd5da653cc09d67ad6fecf50e
Anurag Mohanty <amohanty@amplify.com>
2014-08-04 14:13:05 -0400

    added a basic spec test

--- lib/rubyspark.rb
+++ lib/rubyspark.rb
@@ -1,5 +1,29 @@
 require "rubyspark/version"
-
 module Rubyspark
-  # Your code goes here...
+  class Spark
+
+    def initialize(data=[])
+      @data = []
+      data.cycle(1) {|x| @data.push(x.to_i)}
+      graphit
+    end
+
+    def graphit
+     ticks=%w[▁ ▂ ▃ ▄ ▅ ▆ ▇]
+
+      distance = @data.compact.max.to_f / ticks.size
+
+      graph = ''
+      @data.each do |x|
+        tick = (x/distance).round - 1
+        if tick <= 0 
+          graph << ticks[0]
+        else
+          graph << ticks[tick]
+        end
+      end
+      puts graph
+    end
+
+  end
 end
