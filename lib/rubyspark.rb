2f1dbf1dc08f573e0be5bc3b630d08d0c95e3851
Anurag Mohanty <amohanty@amplify.com>
2014-12-22 10:07:47 -0500

    adding yard documentation

--- lib/rubyspark.rb
+++ lib/rubyspark.rb
@@ -1,16 +1,24 @@
 require 'rubyspark/version'
 module Rubyspark
   class Spark
+    # a new instance of RubySpark::Spark
+    #
+    # @param data [Array] An array of numbers to build the graph from
     def initialize data = []
       @data = []
       data.cycle(1) { |x| @data.push(x.to_f) }
       graphit
     end
 
+    # Compute x's location in relation to the max
+    #
+    # @param x [Fixnum] data point from input array
+    # @param distance [Fixnum] the max/size of ticks array
     def compute x, distance
       (x / distance).round - 1
     end
 
+    # Graphs data, takes data and maps it against flat map of ticks
     def graphit
       ticks = %w(▁ ▂ ▃ ▄ ▅ ▆ ▇)
       distance = @data.max.to_f / ticks.size
